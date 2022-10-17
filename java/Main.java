c906a2cb4c46048fc98605f72ae5442f533bae27
David Gomez <doga19941@hotmail.com>
2022-10-16 22:48:57 -0500

    usando clases en java

--- java/Main.java
+++ java/Main.java
@@ -2,5 +2,10 @@ package java;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "jmr233";
+        car.driver = "Ivan";
+        car.passenger = 4;
+        System.out.println("Car license: " + car.license);
     }
 }

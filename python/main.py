9f44509fd30688d56bf282dffcc3cb46e774f0db
David Gomez <doga19941@hotmail.com>
2022-10-16 22:57:13 -0500

    Uso de clases en python

--- python/main.py
+++ python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car()
+    car.license = "sdw522"
+    car.driver = "David"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "dse587"
+    car2.driver = "Luisa"
+    print(vars(car2))

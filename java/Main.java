2b409881700b207e181b4c94ad200bf35c5f74f6
David Gomez <doga19941@hotmail.com>
2022-10-20 22:30:41 -0500

    Metodos constructivos en java

--- java/Main.java
+++ java/Main.java
@@ -2,9 +2,7 @@ package java;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car();
-        car.license = "jmr233";
-        car.driver = "Ivan";
+        Car car = new Car("jmr233", new Account("David", "Dav587"));
         car.passenger = 4;
         car.printDataCar();
     }
